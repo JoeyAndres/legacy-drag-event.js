37a96ece96c967671d64183a779381b0a4f0fd8b
Joey Arnold <jandres@ualberta.ca>
2016-01-26 23:42:33 -0700

    meteor and update doc.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # legacy-drag.js
-Attaches a 'legacy-drag' event on element when being dragged.
+Attaches a 'legacy-drag:start', 'legacy-drag:dragging', 'legacy-drag:end' event on element when being dragged.
+
+# Installation:
+Stick legacy-drag.js somewhere in your project.
+
+For Meteor:
+```bash
+meteor add jandres:legacy-drag
+```
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+    name: "jandres:legacy-drag",
+    summary: "Attaches a 'legacy-drag:start', 'legacy-drag:dragging', 'legacy-drag:end' event on element when being dragged.",
+    version: "0.1.0",
+    git: "https://github.com/JoeyAndres/legacy-drag.js.git"
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom("1.2.1");
+    api.use([
+        "ecmascript"
+    ], 'client');
+
+    api.addFiles([
+        "legacy-drag.js"
+    ]);
+});
